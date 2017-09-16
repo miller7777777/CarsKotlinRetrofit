ef6d43393acbc488823bfdf0ebad1a16af4e8da8
miller7777777 <miller777@mail.ru>
2017-09-16 15:26:30 +0300

    Прикрутил RX.
    Код работает.

--- app/src/main/java/mnz/creatori/carskotlinretrofit/Api/CarsApi.kt
+++ app/src/main/java/mnz/creatori/carskotlinretrofit/Api/CarsApi.kt
@@ -1,5 +1,6 @@
 package mnz.creatori.carskotlinretrofit.Api
 
+import io.reactivex.Observable
 import mnz.creatori.carskotlinretrofit.Model.CarsResponse
 import retrofit2.Call
 import retrofit2.http.GET
@@ -8,5 +9,6 @@ import retrofit2.http.GET
 interface CarsApi {
 
     @GET("1tW53X")
-    fun getData(): Call<CarsResponse>
+//    fun getData(): Call<CarsResponse>
+    fun getData(): Observable<CarsResponse>
 }
--- app/src/main/java/mnz/creatori/carskotlinretrofit/MainActivity.kt
+++ app/src/main/java/mnz/creatori/carskotlinretrofit/MainActivity.kt
@@ -3,6 +3,9 @@ package mnz.creatori.carskotlinretrofit
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 import mnz.creatori.carskotlinretrofit.Api.CarsApi
 import mnz.creatori.carskotlinretrofit.Model.Car
@@ -17,6 +20,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 class MainActivity : AppCompatActivity() {
 
     var carlist = ArrayList<Car>()
+    val compositeDisposable: CompositeDisposable = CompositeDisposable()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,30 +37,55 @@ class MainActivity : AppCompatActivity() {
         val carsApi: CarsApi = retrofit.create(CarsApi::class.java)
 
 
-        carsApi.getData().enqueue(object : Callback<CarsResponse>{
-            override fun onResponse(call: Call<CarsResponse>, response: Response<CarsResponse>) {
+        compositeDisposable.add(
+
+        carsApi.getData().observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe({
+                    result ->
+//                    tv.text(result.cars[0].toString())
+                    tv.setText(result.cars[0].toString())
+                }, {
+                    error ->
+//                    tv.text{error.printStackTrace().toString()}
+                    tv.setText(error.printStackTrace().toString())
+                })
+
+        )
 
 
-                if (response.isSuccessful) {
-                    val carsResponse: CarsResponse = response.body()!!
-
-//                    Log.d("CARS", carsResponse.carslist.size.toString())
-
-                    carlist.addAll(carsResponse!!.cars)
 
 
-                    tv.text = carlist[0].toString()
-                    //                    tv.setText("1");
-                }
-            }
+//        carsApi.getData().enqueue(object : Callback<CarsResponse>{
+//            override fun onResponse(call: Call<CarsResponse>, response: Response<CarsResponse>) {
+//
+//
+//                if (response.isSuccessful) {
+//                    val carsResponse: CarsResponse = response.body()!!
+//
+////                    Log.d("CARS", carsResponse.carslist.size.toString())
+//
+//                    carlist.addAll(carsResponse!!.cars)
+//
+//
+//                    tv.text = carlist[0].toString()
+//                    //                    tv.setText("1");
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<CarsResponse>, t: Throwable) {
+//
+//                tv.text = "Some Error"
+//            }
+//
+//        })
 
-            override fun onFailure(call: Call<CarsResponse>, t: Throwable) {
 
-                tv.text = "Some Error"
-            }
+    }
 
-        })
+    override fun onDestroy() {
 
-
+        compositeDisposable.clear()
+        super.onDestroy()
     }
 }
